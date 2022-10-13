1fab1ca1303342af6a61fbc51e29bc81e3d7e033
Chananya meth <chananyameth+gitlab@gmail.com>
2022-10-13 19:32:03 +0300

    initial commit

---
+++ Game/MainWindow.xaml.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ארבע_בשורה;
+
+namespace Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MyColor currentColor;
+        public Board board;
+        public int moves;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            initVariables();
+        }
+
+        private void initVariables()
+        {
+            currentColor = MyColor.Red;
+            board = new Board();
+            moves = 0;
+
+            ImageBrush brush = new ImageBrush();
+            ImageBrush brush1 = new ImageBrush();
+            brush.ImageSource = new BitmapImage(new Uri("../../src/arrow-down.png", UriKind.Relative));//C:/חנניה/סתם דברים/C# פרוייקטים/ארבע בשורה/Game
+            brush1.ImageSource = new BitmapImage(new Uri("../../src/table.png", UriKind.Relative));
+            c0.Background = brush;
+            c1.Background = brush;
+            c2.Background = brush;
+            c3.Background = brush;
+            c4.Background = brush;
+            c5.Background = brush;
+            c6.Background = brush;
+
+            c0.Visibility = Visibility.Visible;
+            c1.Visibility = Visibility.Visible;
+            c2.Visibility = Visibility.Visible;
+            c3.Visibility = Visibility.Visible;
+            c4.Visibility = Visibility.Visible;
+            c5.Visibility = Visibility.Visible;
+            c6.Visibility = Visibility.Visible;
+
+            tableImage.Background = brush1;
+
+            currColor.Fill = new SolidColorBrush((currentColor == MyColor.Red ? Colors.Red : Colors.Yellow));
+            reset.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private MyColor replaceColor()
+        {
+            if (currentColor == MyColor.Red)
+                currentColor = MyColor.Yellow;
+            else if (currentColor == MyColor.Yellow)
+                currentColor = MyColor.Red;
+
+            currColor.Fill = new SolidColorBrush((currentColor == MyColor.Red ? Colors.Red : Colors.Yellow));
+
+            return currentColor;
+        }
+
+        private void hideButton(int column)
+        {
+            switch (column)
+            {
+                case 0:
+                    c0.Visibility = Visibility.Hidden;
+                    break;
+                case 1:
+                    c1.Visibility = Visibility.Hidden;
+                    break;
+                case 2:
+                    c2.Visibility = Visibility.Hidden;
+                    break;
+                case 3:
+                    c3.Visibility = Visibility.Hidden;
+                    break;
+                case 4:
+                    c4.Visibility = Visibility.Hidden;
+                    break;
+                case 5:
+                    c5.Visibility = Visibility.Hidden;
+                    break;
+                case 6:
+                    c6.Visibility = Visibility.Hidden;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Winner(MyColor c)
+        {
+            if (c == MyColor.None)
+                MessageBox.Show("No one won... :(", "Standoff!", MessageBoxButton.OK, MessageBoxImage.None);
+            else
+                MessageBox.Show("Congratulations! the " + c + " won the " + replaceColor() + "!", "Congratulations, " + c + "!", MessageBoxButton.OK, MessageBoxImage.None);
+            MessageBoxResult res = MessageBox.Show("Do you want another game?", "More?", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (res == MessageBoxResult.Yes)
+                reset_Click(this, new RoutedEventArgs());
+            else
+                this.Close();
+        }
+
+        private bool doTurn(int column)
+        {
+            if (board.highest(column) == 4)
+            {
+                hideButton(column);
+                //MessageBox.Show("Error! This column is full, try others.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                //return true;//continue the game
+            }
+            if (!board.put(column, currentColor))
+            {
+                MessageBox.Show("Error! Can't put the diskit into column number " + column, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            Ellipse disk = new Ellipse();
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+            mySolidColorBrush.Color = (currentColor == MyColor.Red ? Colors.Red : Colors.Yellow);
+            disk.Fill = mySolidColorBrush;
+            disk.Width = 53;
+            disk.Height = 53;
+            Grid.SetZIndex(disk, -1);
+            MainGrid.Children.Add(disk);
+            Grid.SetRow(disk, 7 - board.rowOfFree(column) + 1);//we already inserted the new one
+            Grid.SetColumn(disk, column + 1);
+
+            moves++;
+            movesNr.Content = moves;
+            if (board.checkForWinner(column, -1, currentColor) != MyColor.None)
+                Winner(currentColor);
+            replaceColor();
+
+            if (moves == 42)//the board is full
+                Winner(MyColor.None);
+
+            return true;
+        }
+
+        private void c0_Click(object sender, RoutedEventArgs e)
+        {
+            if (!doTurn(0))
+            {
+                MessageBox.Show("Error! There was a problem somewhere!", "Error!");
+                this.Close();
+            }
+        }
+        private void c1_Click(object sender, RoutedEventArgs e)
+        {
+            if (!doTurn(1))
+            {
+                MessageBox.Show("Error! There was a problem somewhere!", "Error!");
+                this.Close();
+            }
+        }
+        private void c2_Click(object sender, RoutedEventArgs e)
+        {
+            if (!doTurn(2))
+            {
+                MessageBox.Show("Error! There was a problem somewhere!", "Error!");
+                this.Close();
+            }
+        }
+        private void c3_Click(object sender, RoutedEventArgs e)
+        {
+            if (!doTurn(3))
+            {
+                MessageBox.Show("Error! There was a problem somewhere!", "Error!");
+                this.Close();
+            }
+        }
+        private void c4_Click(object sender, RoutedEventArgs e)
+        {
+            if (!doTurn(4))
+            {
+                MessageBox.Show("Error! There was a problem somewhere!", "Error!");
+                this.Close();
+            }
+        }
+        private void c5_Click(object sender, RoutedEventArgs e)
+        {
+            if (!doTurn(5))
+            {
+                MessageBox.Show("Error! There was a problem somewhere!", "Error!");
+                this.Close();
+            }
+        }
+        private void c6_Click(object sender, RoutedEventArgs e)
+        {
+            if (!doTurn(6))
+            {
+                MessageBox.Show("Error! There was a problem somewhere!", "Error!");
+                this.Close();
+            }
+        }
+
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win = new MainWindow();
+            win.Show();
+            this.Close();
+        }
+    }
+}
---
+++ ארבע בשורה/Board.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ארבע_בשורה
+{
+    public enum MyColor
+    {
+        Red = 1,
+        Yellow = 2,
+        None = 3
+    }
+
+    public enum Directions
+    {
+        right = 1,
+        up_right = 2,
+        up = 3,
+        up_left = 4,
+        left = 5,
+        down_left = 6,
+        down = 7,
+        down_right = 8
+    }
+
+    public class Board
+    {
+        // 05 15 25 35 45 55 65
+        // 04 14 24 34 44 54 64
+        // 03 13 23 33 43 53 63
+        // 02 12 22 32 42 52 62
+        // 01 11 21 31 41 51 61
+        // 00 10 20 30 40 50 60
+
+        public MyColor[,] board;
+
+        public Board()
+        {
+            board = new MyColor[7, 6];
+            for (int x = 0; x < 7; x++)
+                for (int y = 0; y < 6; y++)
+                    board[x, y] = MyColor.None;
+        }
+
+        public bool isFull(int column)
+        {
+            if (board[column, 5] != MyColor.None)//the highest is full - the column is full
+                return true;
+            return false;
+        }
+
+        public int rowOfFree(int column)//6 -> full
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                if (board[column, i] == MyColor.None)
+                    return i;
+            }
+            return 6;
+        }
+
+        public bool put(int column, MyColor c)
+        {
+            if (column < 0 || column > 6 || c == MyColor.None || isFull(column))
+                return false;
+            for (int i = 0; i < 6; i++)
+            {
+                if (board[column, i] == MyColor.None)
+                {
+                    board[column, i] = c;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public MyColor checkForWinner()
+        {
+            for (int y = 0; y < 6; y++)
+            {
+                for (int x = 0; x < 7; x++)
+                {
+                    MyColor c = checkForWinner(x, y);
+                    if (c != MyColor.None)
+                        return c;
+                }
+            }
+            return MyColor.None;
+        }
+
+        public int highest(int x)
+        {
+            for (int y = 5; y >= 0; y--)
+            {
+                if (board[x, y] != MyColor.None)
+                    return y;
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// checks if this disk makes someone winner
+        /// </summary>
+        /// <param name="x">x value</param>
+        /// <param name="y">y value (-1 is the highest)</param>
+        /// <param name="c">color to check</param>
+        /// <returns>which color won</returns>
+        public MyColor checkForWinner(int x, int y = -1, MyColor c = MyColor.None)
+        {
+            if (y == -1)
+                y = highest(x);
+            if (c == MyColor.None)
+                c = board[x, y];
+
+            int c_fit_xy = (c == board[x, y] ? 1 : 0);
+
+            List<int> counts = new List<int>();
+            counts.Add(count(x, y, c, (int)Directions.up) + count(x, y, c, (int)Directions.down) - c_fit_xy);//up_down
+            counts.Add(count(x, y, c, (int)Directions.right) + count(x, y, c, (int)Directions.left) - c_fit_xy);//right_left
+            counts.Add(count(x, y, c, (int)Directions.up_right) + count(x, y, c, (int)Directions.down_left) - c_fit_xy);//positive_slant
+            counts.Add(count(x, y, c, (int)Directions.up_left) + count(x, y, c, (int)Directions.down_right) - c_fit_xy);//negative_slant
+
+            if (counts.Max() >= 4)
+                return c;
+            else
+                return MyColor.None;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x">x value</param>
+        /// <param name="y">y value</param>
+        /// <param name="c">which MyColor</param>
+        /// <param name="direction">1=right, 2=up-right, 3=up, 4=up-left, 5=left, 6=down-left, 7=down, 8=down-right</param>
+        /// <returns>how many disks with the same MyColor in a row</returns>
+        private int count(int x, int y, MyColor c, int direction)
+        {
+            if (x < 0 || x > 6 || y < 0 || y > 5 || c == MyColor.None || board[x, y] != c)
+                return 0;
+
+            switch (direction)
+            {
+                case 1://right
+                    return 1 + count(x + 1, y + 0, c, direction);
+                case 2://up-right
+                    return 1 + count(x + 1, y + 1, c, direction);
+                case 3://up
+                    return 1 + count(x + 0, y + 1, c, direction);
+                case 4://up-left
+                    return 1 + count(x - 1, y + 1, c, direction);
+                case 5://left
+                    return 1 + count(x - 1, y + 0, c, direction);
+                case 6://down-left
+                    return 1 + count(x - 1, y - 1, c, direction);
+                case 7://down
+                    return 1 + count(x + 0, y - 1, c, direction);
+                case 8://down-right
+                    return 1 + count(x + 1, y - 1, c, direction);
+                default:
+                    return 0;
+            }
+        }
+    }
+}
